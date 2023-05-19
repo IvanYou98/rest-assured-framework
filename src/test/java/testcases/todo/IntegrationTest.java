547fb9a5da136a2bd4a43efab66c4698c4de6b66
Ivan You <yuanfanyou@gmail.com>
2023-05-19 17:54:55 -0400

    Init commit

--- src/test/java/testcases/todo/IntegrationTest.java
+++ src/test/java/testcases/todo/IntegrationTest.java
@@ -1,2 +1,59 @@
-package testcases.todo;public class IntegrationTest {
+package testcases.todo;
+
+import api.ToDoListAPI;
+import com.github.javafaker.Faker;
+import entity.ToDo;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class IntegrationTest {
+
+    private static final Faker faker = new Faker();
+    private static Long userId;
+    private static Long todoId;
+    private static final ResponseSpecification schemaValidator = new ResponseSpecBuilder()
+            .expectBody(matchesJsonSchemaInClasspath("todo-schema.json"))
+            .build();
+
+
+    @Test(priority = 0)
+    public void testGetList() {
+        List<ToDo> todoLists = ToDoListAPI
+                .getList()
+                .then().statusCode(200)
+                .extract().jsonPath().getList("", ToDo.class);
+        todoLists.forEach(todo -> matchesJsonSchemaInClasspath("todo-schema.json").matches(todo));
+        userId = todoLists.get(0).getUserId();
+    }
+
+    @Test(priority = 1)
+    public void testCreate() {
+        Response res = ToDoListAPI.create(ToDo.builder()
+                .userId(userId)
+                .status(faker.options().option("completed", "pending"))
+                .title(faker.book().title())
+                .dueOn(faker.date().future(10, TimeUnit.DAYS).toString())
+                .build());
+        todoId = res.then().spec(schemaValidator)
+                .statusCode(201)
+                .extract().jsonPath().getLong("id");
+    }
+
+    @Test(priority = 2)
+    public void testGetDetail() {
+        ToDoListAPI.getDetail(todoId)
+                .then()
+                .spec(schemaValidator);
+    }
+
+
+
+
 }
--- src/test/java/utils/YamlReader.java
+++ src/test/java/utils/YamlReader.java
@@ -1,2 +1,37 @@
-package utils;public class Yaml {
+package utils;
+
+import org.testng.annotations.Parameters;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+public class YamlReader {
+    private static final Map<String, Object> yamlData;
+
+    static {
+        try {
+            yamlData = new Yaml().load(new FileInputStream("src/test/resources/application.yml"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getValue(String key) {
+        Map<String, Object> node = yamlData;
+        String[] keys = key.split("\\.");
+        for (int i = 0; i < keys.length; i++) {
+            if (i == keys.length - 1) return (String) node.get(keys[i]);
+            node = (Map<String, Object>) node.get(keys[i]);
+            if (node == null) return null;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String s = getValue("url.todo") + getValue("path.todo.update");
+        System.out.println(s);
+
+    }
 }
